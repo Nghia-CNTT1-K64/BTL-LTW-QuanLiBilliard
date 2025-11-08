b66f275dd85fcf0a784cd1eea7a145ac99d6c03c
Hoang Van Nghia <nghiahoang14022005@gmail.com>
2025-11-08 21:19:52 +0700

    first

---
+++ BaiTapLon_LapTrinhWeb_QuanLiBilliard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BaiTapLon_LapTrinhWeb_QuanLiBilliard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
